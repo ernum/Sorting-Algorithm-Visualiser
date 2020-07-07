15cf595e11f516fef255e78161f264fb2af9bf41
umeh <umeh@kth.se>
2020-07-07 22:28:22 +0200

    Add columns to bottom and change colour

--- src/main.py
+++ src/main.py
@@ -6,15 +6,17 @@ environ['SDL_VIDEO_WINDOW_POS'] = "500,200"
 py.init()
 
 DISPLAY = py.display.set_mode((800, 600))
+DISPLAY.fill((49, 51, 53))
 py.display.set_caption("Visualising Sorting Algorithms")
 py.display.set_icon(py.image.load("images/icon.png"))
 
 # Values
 run = True
-arr_size = 10
-arr_range = [0, 100]
+arr_size = 25
+arr_range = [50, 300]
 rec_width = 10
-start_pos = 200
+start_pos = 150
+border_width = 4
 
 arr = randint(arr_range[0], arr_range[1], arr_size)
 recs = dict()  # Dictionary contains the number and it's rectangle
@@ -26,15 +28,13 @@ while run:
             run = False
 
     for i in range(arr_size):
-        recs[arr[i]] = py.Rect(start_pos, 100, rec_width, arr[i]*2)
-        py.draw.rect(DISPLAY, (255, 255, 255),
-                     (start_pos, 100, rec_width, arr[i]*2))
-        start_pos += rec_width
+        recs[arr[i]] = py.Rect(start_pos, 0, rec_width, arr[i])
+        recs[arr[i]].bottom = 600
+        py.draw.rect(DISPLAY, (0, 153, 76),
+                     recs[arr[i]])
+        start_pos += rec_width + border_width
+
+    start_pos = 300
     py.display.update()
 
-print(recs)
-print(arr)
-
-for val in recs.values():
-    print(val)
 py.quit()
