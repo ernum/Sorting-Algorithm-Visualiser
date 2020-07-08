6cc1219a8f24e41c20512c05fe89f159915f3331
unknown <umeh@kth.se>
2020-07-08 20:51:45 +0200

    fix issue

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import pygame as py
 from numpy.random import randint
 from os import environ
+from button import Button
 
 environ['SDL_VIDEO_WINDOW_POS'] = "500,200"
 py.init()
 
 screen = py.display.set_mode((800, 700))
-screen.fill((49, 51, 53))
 
 py.display.set_caption("Visualising Sorting Algorithms")
 py.display.set_icon(py.image.load("images/icon.png"))
+
 
 # Values
 run = True
@@ -19,21 +20,42 @@ rec_width = 10
 start_pos = 50
 border_width = 2
 
-arr = randint(arr_range[0], arr_range[1], arr_size)
+
+def gen_arr_btn_action():
+    global arr, arr_visualised
+    arr_visualised = False
+    arr = randint(arr_range[0], arr_range[1], arr_size)
+
+
+settings = {
+    'font': py.font.Font(None, 16),
+    'font_colour': (255, 255, 255),
+    'border_colour': (0, 0, 0),
+}
+
+arr, arr_visualised = randint(arr_range[0], arr_range[1], arr_size), False
 recs = dict()  # Dictionary contains the number and it's rectangle
+gen_arr_btn = Button(rect=(10, 10, 115, 25),
+                     click_action=gen_arr_btn_action, text='Generate New Array', **settings)
 
 while run:
 
     for event in py.event.get():
         if event.type == py.QUIT:
             run = False
+        gen_arr_btn.get_event(event)
 
-    for i in range(arr_size):
-        recs[arr[i]] = py.Rect(start_pos, 0, rec_width, arr[i]*2)
-        recs[arr[i]].bottom = 700
-        py.draw.rect(screen, (0, 153, 76),
-                     recs[arr[i]])
-        start_pos += rec_width + border_width
+    gen_arr_btn.draw(screen)
+
+    if arr_visualised == False:
+        screen.fill((49, 51, 53))
+        for i in range(arr_size):
+            recs[arr[i]] = py.Rect(start_pos, 0, rec_width, arr[i]*2)
+            recs[arr[i]].bottom = 700
+            py.draw.rect(screen, (0, 153, 76),
+                         recs[arr[i]])
+            start_pos += rec_width + border_width
+        arr_visualised = True
 
     start_pos = 50
     py.display.update()
